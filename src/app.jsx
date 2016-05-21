fee10881ce43c50b6e6a65a3a963cd5f44c3372c
Krzyrok <krzyrok@gmail.com>
2016-05-22 01:10:06 +0200

    removed test files (old components)

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { Component } from "react";
 import Component1 from "./component1.jsx";
 import Component2 from "./component2.jsx";
 
-class App extends React.Component {
+class App extends Component {
     render () {
         return (
             <div>
