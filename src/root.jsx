c5c31c9397c60bc19005c6470739bf10ea84137c
Krzyrok <krzyrok@gmail.com>
2016-05-22 04:13:02 +0200

    changed extension of asyncAppContainer from jsx to the js

--- src/root.jsx
+++ src/root.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Provider } from "react-redux";
 import configureStore from "./configureStore.js";
-import AsyncAppContainer from "./asyncAppContainer.jsx";
+import AsyncAppContainer from "./asyncAppContainer.js";
 
 const store = configureStore();
 
